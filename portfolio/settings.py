83ddfdb00690b23b2e941e68b6e3ddef5a06c6ce
abhishek kumar <ruhelabhishek@gmail.com>
2024-06-04 13:03:11 +0530

    changes in setting file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,9 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL='media/'
+
+STATICFILES_DIRS=[
+   BASE_DIR / "static",
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
