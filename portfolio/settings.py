bf5d9e09e83d68a33718a2f8b38597f169437180
abhishek kumar <ruhelabhishek@gmail.com>
2024-06-05 18:07:21 +0530

    changes into the fiel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +82,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -120,16 +123,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
-MEDIA_URL='media/'
-
-STATICFILES_DIRS=[
-   BASE_DIR / "static",
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 
 
 
